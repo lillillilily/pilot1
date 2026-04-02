fa43a37444a315ea7a5e139957b002e27d63ad93
ljm <99.jmgm@gmail.com>
2026-04-02 17:26:12 +0900

    0402

---
+++ js/survey-config.js
@@ -0,0 +1,13 @@
+window.SURVEY_CONFIG = {
+    imageFolder: './images/',
+    imagePattern: 'scene{scene}_B{blur}.png',
+    numScenes: 5,
+    blurLevels: [0, 1, 2, 3, 4],
+    groundTruth: {
+        1: 'right',
+        2: 'left',
+        3: 'up',
+        4: 'down',
+        5: 'right'
+    }
+};
